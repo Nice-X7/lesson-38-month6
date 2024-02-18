020673e1da6dd2bfce03548ca2d21abd0b41398d
Nice_X <777soer@gmail.com>
2024-02-18 22:30:56 +0300

    information from jsonplaseholder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+export const App = () => {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => {
+        return response.json()
+      }).then((json) => {
+        setData(json)
+        setLoading(false)
+      })
+      .catch(e => console.error(e))
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Набор данных</h1>
+      <div className="content">
+        {
+          loading ? "Идет загрузка данных..." : data.map(element => {
+            return (
+              <div className="data">
+                {element.completed ? <span>{element.title}</span> : ""}
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
-
-export default App;
